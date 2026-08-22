545f110bb0d92f189fe5689a18d2acfbaddae33a
AsahiRei <asahirei123@gmail.com>
2026-08-23 00:12:38 +0800

    add

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,10 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativewind } = require("nativewind/metro");
- 
+
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
- 
+
+// register .tflite so Metro bundles it as an asset (like images/fonts) instead of trying to parse it as source
+config.resolver.assetExts.push("tflite");
+
 module.exports = withNativewind(config);
